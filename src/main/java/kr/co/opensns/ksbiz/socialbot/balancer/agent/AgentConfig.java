5b84bc639b9e64f2ce81431e2fad7042a8d960ae
minhow <aeb@nate.com>
2015-10-19 18:28:46 +0900

    클래스 추가

---
+++ src/main/java/kr/co/opensns/ksbiz/socialbot/balancer/agent/AgentConfig.java
@@ -0,0 +1,5 @@
+package kr.co.opensns.ksbiz.socialbot.balancer.agent;
+
+public class AgentConfig {
+
+}
---
+++ src/main/java/kr/co/opensns/ksbiz/socialbot/balancer/seed/SeedLoader.java
@@ -0,0 +1,5 @@
+package kr.co.opensns.ksbiz.socialbot.balancer.seed;
+
+public class SeedLoader {
+
+}
