6173c17e51a088f3ebb6de9b0b499019f298a6d8
ljhsecret <ljhsecret@gmail.com>
2015-12-18 15:03:58 +0900

    Seed Insert 로직 추가
    Instagram 수집후 발견된 Seed를 저장하기 위함.

--- src/main/java/kr/co/opensns/ksbiz/socialbot/crawler/instagram/MediaFetcher.java
+++ src/main/java/kr/co/opensns/ksbiz/socialbot/crawler/instagram/MediaFetcher.java
@@ -1,19 +1,31 @@
 package kr.co.opensns.ksbiz.socialbot.crawler.instagram;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import kr.co.opensns.ksbiz.socialbot.balancer.db.mybatis.ConnectionFactory;
+import kr.co.opensns.ksbiz.socialbot.balancer.seed.SeedDO;
 import kr.co.opensns.ksbiz.socialbot.crawler.fetcher.ApiFetcher;
 import kr.co.opensns.ksbiz.socialbot.crawler.fetcher.Fetcher;
 import kr.co.opensns.ksbiz.socialbot.crawler.instagram.auth.AccessToken;
+import kr.co.opensns.ksbiz.socialbot.crawler.instagram.domain.Media;
 import kr.co.opensns.ksbiz.socialbot.crawler.instagram.domain.Medias;
 import kr.co.opensns.ksbiz.socialbot.internal.http.HttpClient;
 import kr.co.opensns.ksbiz.socialbot.internal.http.HttpClientSimple;
 
 public class MediaFetcher extends ApiFetcher {
 	private AccessToken accessToken;
-	
+	private ConnectionFactory connectionFactory = new ConnectionFactory();
+	public final static int SITE_ID = 1;
+	public final static int SEED_TYPE = 2;
+	public final static int CRL_TYPE = 1;
+	public final static int CHANNEL_ID = 1;
 	public void init() {
 		
 	}
@@ -61,7 +73,19 @@ public class MediaFetcher extends ApiFetcher {
 	    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
 	    .create();
 		Medias medias = gson.fromJson(jsonStr, Medias.class);
+		
+		saveNewSeed(medias);
+		
 		return medias;
 	}
 	
+	public void saveNewSeed(Medias medias) {
+		List<SeedDO> newSeeds = new ArrayList<SeedDO>();
+		for( Iterator<Media> iter = medias.getData().iterator(); iter.hasNext(); ) {
+			Media media = iter.next();
+			if( media.getId() == null ) continue;
+			connectionFactory.insertSeedInfo(new SeedDO(media.getId(), SITE_ID, SEED_TYPE, CRL_TYPE, CHANNEL_ID, "", "", "", new Date(System.currentTimeMillis()), 0, new Date(System.currentTimeMillis()), "", "", "", ""));
+//			System.out.println("new media id" +  media.getId());
+		}
+	}
 }
