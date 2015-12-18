1a350d9ba33207d120608a9d5ce73913b0786b3b
ljhsecret <ljhsecret@gmail.com>
2015-12-18 15:04:42 +0900

    CommentFetcher, LikeFetcher 클래스 생성
    수집 형태별로 Fetcher를 분리하여야 하므로 Fetcher 생성. 로직 없음

---
+++ src/main/java/kr/co/opensns/ksbiz/socialbot/crawler/instagram/CommentFetcher.java
@@ -0,0 +1,25 @@
+package kr.co.opensns.ksbiz.socialbot.crawler.instagram;
+
+import kr.co.opensns.ksbiz.socialbot.crawler.fetcher.ApiFetcher;
+
+public class CommentFetcher extends ApiFetcher{
+
+	@Override
+	public Object fetch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object fetch(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/kr/co/opensns/ksbiz/socialbot/crawler/instagram/LikeFetcher.java
@@ -0,0 +1,25 @@
+package kr.co.opensns.ksbiz.socialbot.crawler.instagram;
+
+import kr.co.opensns.ksbiz.socialbot.crawler.fetcher.ApiFetcher;
+
+public class LikeFetcher extends ApiFetcher {
+
+	@Override
+	public Object fetch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object fetch(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
